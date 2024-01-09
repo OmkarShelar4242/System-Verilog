dcccb3a0311def8b5cd58a8d7b75297bbcb38da0
OmkarShelar4242 <144466924+OmkarShelar4242@users.noreply.github.com>
2024-01-08 21:12:19 -0500

    Create interface.sv

---
+++ UVM/interface.sv
@@ -0,0 +1,11 @@
+// interfacing the DUT
+
+interface alu_interface(input logic clock);
+  
+  logic reset;
+  logic [7:0] a,b;
+  logic [3:0] op_code;
+  logic [7:0] result;
+  bit carryout;
+  
+endinterface: alu_interface
